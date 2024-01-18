d3f96f303672b7b0cf9c94c0cd5515b913468cad
Loke <nguyenvanminhcuong2002@gmail.com>
2024-01-18 17:02:38 +0700

    VGG16_classification

--- trainer.py
+++ trainer.py
@@ -5,19 +5,20 @@ from tqdm import tqdm
 
 
 class Trainer:
-    def __init__(self, model):
+    def __init__(self, model, device="cpu"):
         self.model = model
-        self.metric = Accuracy(task="multiclass", num_classes=model.num_classes)
+        self.device = device
+        self.metric = Accuracy(task="multiclass", num_classes=model.num_classes).to(self.device)
         self.criterion = nn.CrossEntropyLoss()
 
     def configure_optimizers(self):
         return Adam(self.model.parameters(), lr=0.0001)
 
-    def training_step(self, batch, device):
+    def training_step(self, batch):
         self.model.train()
         images, labels = batch
-        images = images.to(device)
-        labels = labels.to(device)
+        images = images.to(self.device)
+        labels = labels.to(self.device)
 
         output = self.model(images)
         loss = self.criterion(output, labels)
@@ -29,11 +30,11 @@ class Trainer:
 
         return loss, metrics
 
-    def validation_step(self, batch, device):
+    def validation_step(self, batch):
         self.model.eval()
         images, labels = batch
-        images = images.to(device)
-        labels = labels.to(device)
+        images = images.to(self.device)
+        labels = labels.to(self.device)
 
         output = self.model(images)
         loss = self.criterion(output, labels)
@@ -51,9 +52,9 @@ class Trainer:
         for epoch in range(max_epoch):
             print(f"___ Epoch {epoch + 1}/{max_epoch} ___")
             for batch in tqdm(train_loader):
-                loss, metrics = self.training_step(batch, device)
+                loss, metrics = self.training_step(batch)
             print(f"Training: Loss: {loss:.4f}, Accuracy: {metrics:.4f}")
 
             for val_batch in tqdm(val_loader):
-                val_loss, val_metrics = self.validation_step(val_batch, device)
+                val_loss, val_metrics = self.validation_step(val_batch)
             print(f"Validation: Loss: {val_loss:.4f}, Accuracy: {val_metrics:.4f}\n")
