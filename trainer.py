e1177fbf392fad2feed64a1f4850bc2905e602f2
Loke <nguyenvanminhcuong2002@gmail.com>
2024-01-18 16:38:19 +0700

    VGG16_classification

---
+++ trainer.py
@@ -0,0 +1,55 @@
+from torchmetrics import Accuracy
+from torch import nn
+from torch.optim import Adam
+from tqdm import tqdm
+
+
+class Trainer:
+    def __init__(self, model):
+        self.model = model
+        self.metric = Accuracy(task="multiclass", num_classes=model.num_classes)
+        self.criterion = nn.CrossEntropyLoss()
+
+    def configure_optimizers(self):
+        return Adam(self.model.parameters(), lr=0.0001)
+
+    def training_step(self, batch):
+        self.model.train()
+        images, labels = batch
+        output = self.model(images)
+        loss = self.criterion(output, labels)
+        metrics = self.metric(output, labels)
+
+        self.configure_optimizers().zero_grad()
+        loss.backward()
+        self.configure_optimizers().step()
+
+        return loss, metrics
+
+    def validation_step(self, batch):
+        self.model.eval()
+        images, labels = batch
+        output = self.model(images)
+        loss = self.criterion(output, labels)
+        metrics = self.metric(output, labels)
+
+        return loss, metrics
+
+    def fit(self, max_epoch, train_loader, val_loader, device):
+        loss = 0
+        metrics = 0
+        val_loss = 0
+        val_metrics = 0
+        self.model.to(device)
+
+        for epoch in range(max_epoch):
+            print(f"___ Epoch {epoch + 1}/{max_epoch} ___")
+            for batch in tqdm(train_loader):
+                batch = batch.to(device)
+                loss, metrics = self.training_step(batch)
+            print(f"Training: Loss: {loss:.4f}, Accuracy: {metrics:.4f}")
+
+            for val_batch in tqdm(val_loader):
+                val_batch = val_batch.to(device)
+                val_loss, val_metrics = self.validation_step(val_batch)
+            print(f"Validation: Loss: {val_loss:.4f}, Accuracy: {val_metrics:.4f}\n")
---
+++ training.py
@@ -0,0 +1,34 @@
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import random_split, DataLoader
+from trainer import Trainer
+from model import VGG16
+import torch
+
+
+if __name__ == "__main__":
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    path = "./PetImg"
+    train_size = 20000
+    val_size = 5000
+
+    transforms = transforms.Compose([transforms.ToTensor(),
+                                     transforms.Resize((224, 224), antialias=True),
+                                     transforms.Normalize((0.5,), (0.5,)),
+                                     transforms.RandomPerspective(distortion_scale=0.5, p=0.1),
+                                     transforms.RandomRotation(degrees=(0, 180)),
+                                     transforms.RandomHorizontalFlip(p=0.5),
+                                     transforms.RandomVerticalFlip(p=0.5)])
+
+    dataset = ImageFolder(path, transform=transforms)
+    train_ds, val_ds = random_split(dataset, [train_size, val_size])
+
+    train_dl = DataLoader(train_ds, batch_size=32, shuffle=True, num_workers=2)
+    val_dl = DataLoader(val_ds, batch_size=32, shuffle=False, num_workers=2)
+
+    model = VGG16(num_classes=2)
+
+    trainer = Trainer(model)
+    trainer.fit(max_epoch=30, train_loader=train_dl, val_loader=val_dl, device=device)
