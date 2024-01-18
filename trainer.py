e4b0614c115ae5edc027af0f200ae48b03af4333
Loke <nguyenvanminhcuong2002@gmail.com>
2024-01-18 16:55:44 +0700

    VGG16_classification

--- trainer.py
+++ trainer.py
@@ -51,9 +51,9 @@ class Trainer:
         for epoch in range(max_epoch):
             print(f"___ Epoch {epoch + 1}/{max_epoch} ___")
             for batch in tqdm(train_loader):
-                loss, metrics = self.training_step(batch)
+                loss, metrics = self.training_step(batch, device)
             print(f"Training: Loss: {loss:.4f}, Accuracy: {metrics:.4f}")
 
             for val_batch in tqdm(val_loader):
-                val_loss, val_metrics = self.validation_step(val_batch)
+                val_loss, val_metrics = self.validation_step(val_batch, device)
             print(f"Validation: Loss: {val_loss:.4f}, Accuracy: {val_metrics:.4f}\n")
